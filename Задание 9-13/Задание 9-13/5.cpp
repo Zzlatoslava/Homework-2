d48d6533381521ffeaa361cb7f137afa40ad738b
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-11-06 21:46:21 +0300

    Add files via upload

---
+++ Задание 9-13/Задание 9-13/5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+int main5() {
+    double x1, x2, dx, eps;
+    std::cout << "x1 x2 dx eps: ";
+    std::cin >> x1 >> x2 >> dx >> eps;
+
+
+    std::cout << "x\tf(x)\tsin(x)\n";
+    for (; x1 <= x2; x1 += dx) {
+        std::cout << x1 << "\t";
+        double curr = x1;
+        double sum = 0;
+        int counter = 1;
+        while (std::abs(curr) >= eps) {
+            sum += curr;
+            counter += 2;
+            curr = -curr * x1 * x1 / (counter * (counter - 1));
+
+        }
+        std::cout << sum << "\t" << sin(x1) << "\n";
+
+    }
+    return 0;
+}
