f4f41f095b8557a469e20908cd86b74407f4bd8e
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-12-17 23:13:56 +0300

    Add files via upload

---
+++ Arrays/9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    int N, ArithProg, summ = 0;
+    std::cout << "Enter the number of numbers: "; 
+    std::cin >> N;
+    std::vector <int> A(N);
+    ArithProg = ((2 + N - 1) * N) / 2;    // Находим сумму всех элементов если бы не было пропуска 
+    std::cout << "Enter sequence: \n";
+    for (int i = 0; i < N; i++) {
+        std::cin >> A[i];
+        ArithProg -= A[i];
+    }
+    std::cout << "Missing number: " << ArithProg ;
+    return 0;
+
+}
