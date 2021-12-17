2ec4e1faec926e8d1ca341154689d639f5217f64
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-12-17 23:17:19 +0300

    Update 9.cpp

--- Arrays/9.cpp
+++ Arrays/9.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 
 int main() {
+    setlocale(LC_ALL, "RUS");
     int N, ArithProg, summ = 0;
     std::cout << "Enter the number of numbers: "; 
     std::cin >> N;
     std::vector <int> A(N);
-    ArithProg = ((2 + N - 1) * N) / 2;    // Находим сумму всех элементов если бы не было пропуска 
+    ArithProg = ((2 + N - 1) * N) / 2;    // РќР°С…РѕРґРёРј СЃСѓРјРјСѓ РІСЃРµС… СЌР»РµРјРµРЅС‚РѕРІ РµСЃР»Рё Р±С‹ РЅРµ Р±С‹Р»Рѕ РїСЂРѕРїСѓСЃРєР° 
     std::cout << "Enter sequence: \n";
     for (int i = 0; i < N; i++) {
         std::cin >> A[i];
