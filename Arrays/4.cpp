be4199813510a478ab34f72024a156dbd0f21ad2
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-12-17 20:40:55 +0300

    Add files via upload

---
+++ Arrays/4.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream> 
+#include <vector>
+
+int main4() {
+    int N;
+    std::cin >> N;
+    std::vector <int> A(N); 
+    for (int i = 0; i < N; i++) {
+        std::cin >> A[i]; 
+    }
+        for (int p = 1; p < N + 3; p++) {
+            std::cout << A[p - 1] << " " << A[p + N - 2] << " ";
+            N -= 2;
+        }
+    
+    }*/
