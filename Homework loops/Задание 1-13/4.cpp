ea97ce6d0d5ff5c2f2dd2c6481d50614159f46b3
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-12-18 23:02:42 +0300

    Update 4.cpp

--- Homework loops/Задание 1-13/4.cpp
+++ Homework loops/Задание 1-13/4.cpp
@@ -1,20 +1,26 @@
-#include <iostream>
-
-int main_4 () {
-
-    double pi, N, dx, x;
-    std::cout << "Please enter the required accuracy\n";
-    std::cin >> dx;
-    N = 0;
-    pi = 4;
-    x = 1;
-    while (fabs(pi) >= dx) {
-        N += 1;
-        pi += 4 / x;
-        x += 2;
-    }
-    std::cout << N << pi;
-    return 0;
+#include <iostream> 
+ 
+double get_nth_term(unsigned long long n) { 
+    double nth_term = 1 / (double)(2 * n - 1); 
+    if (n % 2 == 0) { 
+        return -nth_term; 
+    } else { 
+        return nth_term; 
+    } 
+} 
+ 
+int main() { 
+    double precision; 
+    do { 
+        std::cout << "Enter precision (>0): " << std::endl; 
+        std::cin >> precision; 
+    } while (precision <= 0); 
+    unsigned long long n = 0; 
+    double sum = 0, prev_sum; 
+    do { 
+        prev_sum = sum; 
+        sum += get_nth_term(++n); 
+    } while (std::abs(4 * (sum - prev_sum)) > precision); 
+    std::cout << "Pi=" << 4 * sum << std::endl; 
+    std::cout << "Iterations:  " << n; 
 }
-
-//ÍÅÒ ÐÅØÅÍÈß 
