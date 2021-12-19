c1bf7068e371280a75f30f602a3864d3390c3fa3
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-12-19 13:35:11 +0300

    Update 7.cpp

--- Dyn_arrays/7.cpp
+++ Dyn_arrays/7.cpp
@@ -1,30 +1,27 @@
-/*Íàïèñàòü ôóíêöèþ
-void shiftArray(int A[], int n, in k)
-êîòîðàÿ îñóùåñòâëÿåò öèêëè÷åñêèé ñäâèã ýëåìåíòîâ ìàññèâà íà k âëåâî. Íàïðèìåð, åñëè áûë ìàññèâ
-A[10]= {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-Òî ïîñëå âûçîâà ôóíêöèè shiftArray(A, 10, 3); Äîëæåí ïîëó÷èòüñÿ ìàññèâ {3, 4, 5, 6, 7, 8, 9, 0, 1,2};*/
-
-
 #include<iostream>
 
 void shiftArray(int* A, int n, int k) {
+    int* B = new int[n];
     for (int i = 0; i < n; ++i) {
         if (i < k) {
-            A[n - k + i] = i;
+            B[n - k + i] = A[i];
         }
         else {
-            A[i - k] = i;
+            B[i - k] = A[i];
         }
     }
+    for (int i = 0; i < n; ++i) {
+        A[i] = B[i];
+    }
+    delete[] B;
 }
 
 int main() {
-    setlocale (LC__ALL , "RUS");
     int N, z;
     std::cin >> N >> z;
     int* arr = new int[N];
     for (int i = 0; i < N; ++i) {
-        arr[i] = i;
+        std::cin >> arr[i];
     }
     shiftArray(arr, N, z);
     for (int i = 0; i < N; ++i) {
