b7018ed8d5886eebdf1a29b8f82e7ae9d6493a30
Zzlatoslava <90253649+Zzlatoslava@users.noreply.github.com>
2021-12-20 10:03:30 +0300

    Update Reverse words.cpp

--- Codewars/Reverse words.cpp
+++ Codewars/Reverse words.cpp
@@ -1,24 +1,28 @@
+#include <iostream>
 #include <string>
-using namespace std;
-string reverse_words(string str)
-{
-    string result;
-    string word;
-    for (int i = 0; i <= str.size(); i++) {
 
-        if ((str[i] == ' ') || (str[i] == '\0')) {
-            reverse(word.begin(), word.end());
-            if (str[i] != '\0') {
-                result += word + str[i];
-            }
-            else {
-                result += word;
-            }
-            word.clear();
-        }
-        else {
-            word += str[i];
-        }
+std::string reverse_words(std::string str)
+{
+    int q = str.length();
+    int p = 0;
+    int c = 0;
+    char s;
+
+    while (c < q)
+    {
+        while (str[p] == ' ')
+            p++;
+
+        while (c != q - 1 && str[c + 1] != ' ')
+            c++;
+
+        for (int i = p; i < p + (c - p + 1)/2; i++){
+            s =  str[c - (i - p)];
+          str[c - (i - p)] = str[i];
+          str[i] = s;
+}
+        p = ++c;
     }
-    return result;
+
+    return str;
 }
